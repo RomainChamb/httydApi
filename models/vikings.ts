b607a9b0984104b0bc8dd9e710a0c49427c35c0e
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2020-07-21 21:48:05 +0200

    add logger

--- models/vikings.ts
+++ models/vikings.ts
@@ -1,4 +1,5 @@
 import { FileReader } from "../models/fileReader.ts"
+import * as log from "https://deno.land/std@0.61.0/log/mod.ts";
 
 interface Viking {
     id: number;
@@ -14,19 +15,26 @@ interface Viking {
 }
 
 async function retrieveAndConvertData(): Promise<Array<Viking>> {
-    return await new FileReader().readJson("data", "vikings.json") as Array<Viking>;
+    log.info("Retrieving and converting data start...");
+    const vikins = await new FileReader().readJson("data", "vikings.json") as Array<Viking>;
+    log.info(`Retrieving and converting data end...`)
+    return vikins;
 }
 
 export async function filterVikingById(id: number, vikings: Array<Viking>): Promise<Array<Viking>> {
+    log.info(`Filtering vikings with id = ${id}`);
     return vikings.filter((viking: Viking) => viking.id === id);
 }
 
 export async function getOne(id: number): Promise<Viking | Error> {
+    log.info("GetOne start...")
     const vikingsList = await retrieveAndConvertData();
     const viking = await filterVikingById(id, vikingsList);
     if (viking !== null && viking !== undefined && viking.length > 0) {
+        log.info("GetOne end...")
         return viking[0];
     }
+    log.error(`No viking found for the id = ${id}`);
     throw new Error(`The viking with the id: ${id} doesn't exist`);
 }
 
@@ -39,14 +47,18 @@ export async function getAll(): Promise<Array<Viking> | Error> {
 }
 
 export async function getRandom(number: number): Promise<Array<Viking> | Error> {
+    log.info("GetRandom start...");
     const vikings = await retrieveAndConvertData();
     if (number === 0) {
+        log.error("Impossible to search for 0 viking");
         throw new Error("Please choose a number higher than 0");
     } else if (number > vikings.length) {
+        log.error(`${number} is higher than the number of vikings : ${vikings.length}`);
         throw new Error(`Please choose a number higher than 0 and lower than ${vikings.length}`);
     } else if (vikings !== null && vikings !== undefined && vikings.length > 0) {
         return await getRandomVikings(number, vikings);
     }
+    log.error("No available data");
     throw new Error("No Viking found !");
 }
 
@@ -62,6 +74,7 @@ export async function getRandomVikings(number: number, vikings: Array<Viking>): 
             randomVikings.set(randomId, viking[0]);
         }
     }
+    log.info(`${number} random viking(s) found!`);
     return [...randomVikings.values()];
 }
 
