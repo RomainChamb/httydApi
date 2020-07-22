3d466525d1f4a8bc2da2c4ae847f824ba06adb61
Romain Chambonnier <romain.chambonnier+git@protonmail.com>
2020-07-22 21:18:43 +0200

    defining a logger handler

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,29 @@
+import { Application } from "https://deno.land/x/oak@v6.0.1/mod.ts";
+import * as log from "https://deno.land/std@0.61.0/log/mod.ts"
+
+const app = new Application();
+const PORT = 8000;
+
+await log.setup({
+    handlers: {
+        console: new log.handlers.ConsoleHandler("INFO"),
+        file: new log.handlers.FileHandler("ERROR", {
+            filename: "./log/httydLog.txt",
+            formatter: "{datetime} - {levelName} : {msg}",
+            mode: 'a',
+        }),
+    },
+    loggers: {
+        default: {
+            level: "INFO",
+            handlers: ["console", "file"],
+        },
+    },
+});
+
+
+
+
 
 
 
