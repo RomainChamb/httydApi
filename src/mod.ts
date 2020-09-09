50a80827b2a571add742eb3456af2ed26e6517ec
Romain <romain.chambonnier+git@protonmail.com>
2020-09-09 20:51:54 +0200

    Refactor/dependencies (#22)
    * create deps.ts and updating depencies import
    
    * create lock.json

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
-import { Application, Context } from "https://deno.land/x/oak@v6.0.1/mod.ts";
-import * as log from "https://deno.land/std@0.62.0/log/mod.ts";
+import { Application, Context } from "./deps.ts";
+import { log } from "./deps.ts";
 
 import api from "./api.ts";
 
